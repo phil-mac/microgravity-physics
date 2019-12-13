06dafbdbe70d9539d96d594b19d2072d758cfac6
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2019-12-12 18:49:55 -0800

    setup react router with pomodoro page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import './App.css';
+import * as ROUTES from './constants/routes';
+import Navigation from './components/Navigation/index'
+import Home from './components/Home';
+import Pomodoro from './components/Pomodoro';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to MicrogravityPhysics.com</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to MicrogravityPhysics.com</h1>
+          <Navigation />
+          <hr />
+          <Route exact path={ROUTES.HOME} component={Home} />
+          <Route exact path={ROUTES.POMODORO} component={Pomodoro} />
+        </header>
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/constants/routes.js
@@ -0,0 +1,3 @@
+export const HOME = '/';
+export const SIM = '/sim';
+export const POMODORO = '/pomodoro';
